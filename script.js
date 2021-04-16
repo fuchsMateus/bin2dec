e290ec8ed71fe23020761257ff7dc75dd25fcc5a
Mateus Fuchs <mateus5fuchs@gmail.com>
2021-04-16 00:43:03 -0300

    Some validations created

--- script.js
+++ script.js
@@ -8,12 +8,36 @@ const campoResultado = document.getElementById("campo-resultado");
 function trocarConversao(){
     if(convertState == "bin2dec"){
         texto1.textContent = "Valor Decimal"
+        input.value = null;
         campoResultado.textContent = ""
         convertState = "dec2bin";
     }
     else{
         texto1.textContent = "Valor Binário"
+        input.value = null;
         campoResultado.textContent = ""
         convertState = "bin2dec";
     }
+    
 }
+
+function verificaCaracter(){
+    if(convertState == "bin2dec"){
+        if(input.value.slice(-1) != "1" && input.value.slice(-1) != "0"
+        && input.value.slice(-1) != ""){
+                alert("O valor digitado não é binário");
+                input.value = input.value.slice(0, -1);
+        }
+    }
+    else{
+        if(input.value.charAt(0) == "0" && input.value.charAt(1) != ""){
+            alert("O valor digitado não é válido");
+            input.value = input.value.slice(0, -1);
+        }
+    }
+}
+
+function converter(){
+
+}
+
